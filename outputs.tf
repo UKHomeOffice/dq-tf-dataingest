3fc0942b205303529f297aecb467d5744c55367f
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-05 15:56:58 +0000

    not using any of these outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,1 @@
-output "di_subnet_id" {
-  value = "${aws_subnet.data_ingest.id}"
-}
 
-output "di_db_sg" {
-  value = "${aws_security_group.di_db.id}"
-}
-
-output "di_web_sg" {
-  value = "${aws_security_group.di_web.id}"
-}
-
-output "data_ingest_cidr_block" {
-  value = "${var.data_ingest_cidr_block}"
-}
