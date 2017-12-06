014797476723aa3be3dc1f3e0d8057602deabaa6
david-wirelab <info@wirelab.co.uk>
2017-12-06 12:01:44 +0000

    added listeners

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
+output "di_connectivity_tester_web_ip" {
+  value = "${var.di_connectivity_tester_web_ip}"
+}
 
+output "di_connectivity_tester_db_ip" {
+  value = "${var.di_connectivity_tester_db_ip}"
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "data_pipe_apps_cidr_block" {}
 variable "data_ingest_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
+variable "di_connectivity_tester_db_ip" {
+  description = "Mock EC2 database instance."
+  default     = "10.1.4.11"
+}
+
+variable "di_connectivity_tester_web_ip" {
+  description = "Mock EC2 web instance."
+  default     = "10.1.6.21"
+}
