24dc9a322070439cf0727eca710ceb8f080bf770
nick-otter <nicholas.a.otter@gmail.com>
2018-01-08 15:27:29 +0000

    added naming suffix

--- variables.tf
+++ variables.tf
@@ -4,7 +4,11 @@ variable "data_pipe_apps_cidr_block" {}
 variable "data_ingest_cidr_block" {}
 variable "peering_cidr_block" {}
 variable "az" {}
-variable "name_prefix" {}
+
+variable "naming_suffix" {
+  default     = false
+  description = "Naming suffix for tags, value passed from dq-tf-apps"
+}
 
 variable "route_table_id" {
   default     = false
