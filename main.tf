f2b6406297cd72a1b8aa7682c1b74363c899b1e8
nick-otter <nicholas.a.otter@gmail.com>
2018-02-28 15:56:14 +0000

    Added Linux server parameter: ssh_public_key_ssm_wherescape

--- main.tf
+++ main.tf
@@ -238,6 +238,7 @@ fi
 
 sudo -u wherescape sh -c "aws --region eu-west-2 ssm get-parameter --name NATS_sftp_user_private_key --query 'Parameter.Value' --output text --with-decryption | base64 --decode > ~/id_rsa"
 sudo -u wherescape sh -c "aws --region eu-west-2 ssm get-parameter --name maytech_preprod_ssh_private_key --query 'Parameter.Value' --output text --with-decryption | base64 --decode > ~/maytech_preprod_id_rsa"
+sudo -u wherescape sh -c "aws --region eu-west-2 ssm get-parameter --name ssh_public_key_ssm_wherescape	--query 'Parameter.Value' --output text --with-decryption > ~/.ssh/authorized_keys"
 
 sudo touch /etc/profile.d/script_envs.sh
 sudo setfacl -m u:wherescape:rwx /etc/profile.d/script_envs.sh
