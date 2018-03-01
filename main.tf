f39b7c380131351cd12e1f970fedeeb319f5f26b
nick-otter <nicholas.a.otter@gmail.com>
2018-03-01 15:30:39 +0000

    Updated Linux user data mvt formatting

--- main.tf
+++ main.tf
@@ -259,7 +259,7 @@ export MAYTECH_USER=`aws --region eu-west-2 ssm get-parameter --name maytech_use
 export MAYTECH_OAG_LANDING_DIR=`aws --region eu-west-2 ssm get-parameter --name maytech_oag_landing_dir --query 'Parameter.Value' --output text --with-decryption`
 export MAYTECH_OAG_PRIVATE_KEY_PATH="/home/wherescape/maytech_preprod_id_rsa"
 export MVT_SCHEMA_SSM_USERNAME="ssm"
-export MVT_SCHEMA_SSM_PASSWORD=`aws --region eu-west-2 ssm get-parameter --name mvt_schema_ssm_password --query 'Parameter.Value' --output text --with-decryption`" > /etc/profile.d/script_envs.sh
+export MVT_SCHEMA_SSM_PASSWORD="`aws --region eu-west-2 ssm get-parameter --name mvt_schema_ssm_password --query 'Parameter.Value' --output text --with-decryption`"" > /etc/profile.d/script_envs.sh
 
 su -c "/etc/profile.d/script_envs.sh" - wherescape
 
