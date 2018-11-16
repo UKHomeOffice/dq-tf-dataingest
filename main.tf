4d011f681eb359ffe89f3f8d94988b6f043afd5f
david-wirelab <info@wirelab.co.uk>
2018-11-16 11:50:58 +0000

    Change user data again, remove running the env script within the blkock, change execute syntax

--- main.tf
+++ main.tf
@@ -284,14 +284,13 @@ export ADT_SCRIPTS_FOLDER=`aws --region eu-west-2 ssm get-parameter --name linux
 
 su -c "/etc/profile.d/script_envs.sh" - wherescape
 
-sudo -u wherescape sh -c "
-/etc/profile.d/script_envs.sh && \
+su -c "
 cd /appdata/data-transfer && \
 source ~/.virtualenvs/data-transfer/bin/activate && \
 export PYTHONPATH=. && \
 pm2 start -u wherescape ecosystem.config.js -- $DATA_ARCHIVE_BUCKET_NAME $GA_BUCKET_NAME $GA_BUCKET_ACCESS_KEY_ID $GA_BUCKET_SECRET_ACCESS_KEY && \
 pm2 save
-"
+" - wherescape
 
 su -c "/ADT/scripts/acl_oag_download.sh" - wherescape && sed -i 's/#NotRunning#//g' /var/spool/cron/wherescape
 
