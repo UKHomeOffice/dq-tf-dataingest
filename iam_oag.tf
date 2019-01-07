1abe041840f5d23e54dab3c29e64de5788aee531
david-wirelab <info@wirelab.co.uk>
2019-01-07 10:13:29 +0000

    add DynamoDB and permissions to IAM policy

--- iam_oag.tf
+++ iam_oag.tf
@@ -1,38 +1,41 @@
-resource "aws_iam_group" "oag_put" {
-  name = "iam-group-oag-put-${local.naming_suffix}"
+resource "aws_iam_group" "oag" {
+  name = "iam-group-oag-${local.naming_suffix}"
 }
 
-resource "aws_iam_group_membership" "oag_put" {
-  name = "iam-group-membership-oag-put-${local.naming_suffix}"
+resource "aws_iam_group_membership" "oag" {
+  name = "iam-group-membership-oag-${local.naming_suffix}"
 
   users = [
-    "${aws_iam_user.oag_put.name}",
+    "${aws_iam_user.oag.name}",
   ]
 
-  group = "${aws_iam_group.oag_put.name}"
+  group = "${aws_iam_group.oag.name}"
 }
 
-resource "aws_iam_group_policy" "oag_put" {
-  name  = "group-policy-oag-put-${local.naming_suffix}"
-  group = "${aws_iam_group.oag_put.id}"
+resource "aws_iam_group_policy" "oag" {
+  name  = "group-policy-oag-${local.naming_suffix}"
+  group = "${aws_iam_group.oag.id}"
 
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Sid": "ListS3Bucket",
       "Effect": "Allow",
       "Action": "s3:ListBucket",
       "Resource": "${aws_s3_bucket.data_landing_bucket.arn}"
     },
     {
+      "Sid": PutS3Bucket,
+      "Effect": "Allow",
       "Action": [
         "s3:PutObject"
       ],
-      "Effect": "Allow",
       "Resource": "${aws_s3_bucket.data_landing_bucket.arn}/*"
     },
     {
+      "Sid": "UseKMSKey",
       "Effect": "Allow",
       "Action": [
         "kms:Encrypt",
@@ -41,75 +44,41 @@ resource "aws_iam_group_policy" "oag_put" {
         "kms:GenerateDataKey*",
         "kms:DescribeKey"
         ],
-        "Resource": "${aws_kms_key.data_landing_bucket_key.arn}"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_user" "oag_put" {
-  name = "iam-user-oag-put-${local.naming_suffix}"
-}
-
-resource "aws_iam_access_key" "oag_put" {
-  user = "${aws_iam_user.oag_put.name}"
-}
-
-resource "aws_iam_group" "oag_get" {
-  name = "iam-group-oag-get-${local.naming_suffix}"
-}
-
-resource "aws_iam_group_membership" "oag_get" {
-  name = "iam-group-membership-oag-get-${local.naming_suffix}"
-
-  users = [
-    "${aws_iam_user.oag_get.name}",
-  ]
-
-  group = "${aws_iam_group.oag_get.name}"
-}
-
-resource "aws_iam_group_policy" "oag_get" {
-  name  = "group-policy-oag-get-${local.naming_suffix}"
-  group = "${aws_iam_group.oag_get.id}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListBucket",
-      "Resource": "${var.archive_bucket}"
+        "Resource": [
+          "${aws_kms_key.data_landing_bucket_key.arn}",
+          "${aws_dynamodb_table.oag.arn}",
+        ]
     },
-    {
+      "Sid": "ListDynamoDBTable",
+      "Effect": "Allow",
       "Action": [
-        "s3:GetObject"
+        "dynamodb:List*"
       ],
-      "Effect": "Allow",
-      "Resource": "${var.archive_bucket}/*"
+      "Resource": "${aws_dynamodb_table.oag.arn}"
     },
     {
+      "Sid": "InteractDynamoDBTable",
       "Effect": "Allow",
       "Action": [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:DescribeKey"
-        ],
-        "Resource": "${var.apps_buckets_kms_key}"
+        "dynamodb:DescribeTable",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:BatchGet*",
+        "dynamodb:Get*",
+        "dynamodb:BatchWrite*",
+        "dynamodb:PutItem"
+      ],
+      "Resource": "${aws_dynamodb_table.oag.arn}"
     }
   ]
 }
 EOF
 }
 
-resource "aws_iam_user" "oag_get" {
-  name = "iam-user-oag-get-${local.naming_suffix}"
+resource "aws_iam_user" "oag" {
+  name = "iam-user-oag-${local.naming_suffix}"
 }
 
-resource "aws_iam_access_key" "oag_get" {
-  user = "${aws_iam_user.oag_get.name}"
+resource "aws_iam_access_key" "oag" {
+  user = "${aws_iam_user.oag.name}"
 }
