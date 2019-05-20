89b937bf2bf99a84f35dfee7b977466f29e37c38
David Takacs <info@wirelab.co.uk>
2019-05-20 06:47:32 +0100

    Revert "Add CW alerts"
    This reverts commit 4bc96bfad4ff3c41bedfe6a5e52726dccecdc8ef.

--- data.tf
+++ data.tf
@@ -1,1 +1,21 @@
 data "aws_region" "current" {}
+
+data "aws_ssm_parameter" "data-landing-s3" {
+  name = "data-landing-s3"
+}
+
+data "aws_ssm_parameter" "data-landing-kms" {
+  name = "data-landing-kms"
+}
+
+data "aws_ssm_parameter" "ga_bucket" {
+  name = "ga_bucket_name"
+}
+
+data "aws_ssm_parameter" "ga_bucket_id" {
+  name = "gait_access_key"
+}
+
+data "aws_ssm_parameter" "ga_bucket_key" {
+  name = "gait_secret_key"
+}
