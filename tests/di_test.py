517194d3109d2a701630ab7cc25d409696c0c5ff
Duncan Mountford <duncan9562@gmail.com>
2019-06-24 12:47:18 +0100

    Add new test MDS Postgres instance

--- tests/di_test.py
+++ tests/di_test.py
@@ -33,6 +33,7 @@ class TestE2E(unittest.TestCase):
               archive_bucket              = "dq-test"
               archive_bucket_name         = "dq-test"
               apps_buckets_kms_key        = "arn:aws:kms:eu-west-2:123456789:key/654dy74520786elkfugho4576lfk;suh358976"
+              environment                 = "pre-prod"
             }
         """
         self.result = Runner(self.snippet).result
