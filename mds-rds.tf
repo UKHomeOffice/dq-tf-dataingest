ac1bcbaadf7e5b0250d339ad9857a53363aa08c6
David Takacs <info@wirelab.co.uk>
2019-12-04 12:52:38 +0000

    remove MSSQL RDS server

--- mds-rds.tf
+++ mds-rds.tf
@@ -92,39 +92,6 @@ resource "aws_ssm_parameter" "mds_password" {
   value = "${random_string.mds_password.result}"
 }
 
-resource "aws_db_instance" "mds_mssql_2012" {
-  identifier              = "mds-rds-mssql2012-${local.naming_suffix}"
-  allocated_storage       = 200
-  storage_type            = "gp2"
-  engine                  = "sqlserver-se"
-  engine_version          = "11.00.6594.0.v1"
-  license_model           = "license-included"
-  instance_class          = "db.m4.large"
-  username                = "${random_string.mds_username.result}"
-  password                = "${random_string.mds_password.result}"
-  backup_window           = "00:00-01:00"
-  maintenance_window      = "mon:01:30-mon:02:30"
-  backup_retention_period = 14
-  storage_encrypted       = true
-  multi_az                = false
-  skip_final_snapshot     = true
-  ca_cert_identifier      = "${var.environment == "prod" ? "rds-ca-2019" : "rds-ca-2019"}"
-
-  monitoring_interval = "60"
-  monitoring_role_arn = "${var.rds_enhanced_monitoring_role}"
-
-  db_subnet_group_name   = "${aws_db_subnet_group.mds_rds.id}"
-  vpc_security_group_ids = ["${aws_security_group.mds_db.id}"]
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags {
-    Name = "mds-rds-mssql2012-${local.naming_suffix}"
-  }
-}
-
 resource "aws_db_instance" "mds_postgres" {
   identifier                      = "mds-postgres-${local.naming_suffix}"
   allocated_storage               = 200
@@ -157,15 +124,4 @@ resource "aws_db_instance" "mds_postgres" {
   tags {
     Name = "mds-rds-postgres-${local.naming_suffix}"
   }
-}
-
-module "rds_alarms" {
-  source = "github.com/UKHomeOffice/dq-tf-cloudwatch-rds"
-
-  naming_suffix                = "${local.naming_suffix}"
-  environment                  = "${var.naming_suffix}"
-  pipeline_name                = "MDS"
-  db_instance_id               = "${aws_db_instance.mds_mssql_2012.id}"
-  swap_alarm                   = "false"
-  free_storage_space_threshold = 30000000000 # 30GB free space
 }
