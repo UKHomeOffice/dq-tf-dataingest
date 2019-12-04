925100c325956bba12749da10f7369bc735e81fc
David Takacs <info@wirelab.co.uk>
2019-12-04 12:52:38 +0000

    remove MSSQL RDS server

--- tests/di_test.py
+++ tests/di_test.py
@@ -54,14 +54,8 @@ class TestE2E(unittest.TestCase):
     def test_name_suffix_mds_subnet_group(self):
         self.assertEqual(self.result['data_ingest']["aws_db_subnet_group.mds_rds"]["tags.Name"], "mds-rds-subnet-group-dataingest-apps-preprod-dq")
 
-    def test_name_suffix_mds_rds(self):
-        self.assertEqual(self.result['data_ingest']["aws_db_instance.mds_mssql_2012"]["tags.Name"], "mds-rds-mssql2012-dataingest-apps-preprod-dq")
-
     def test_name_suffix_mds_db(self):
         self.assertEqual(self.result['data_ingest']["aws_security_group.mds_db"]["tags.Name"], "sg-mds-db-dataingest-apps-preprod-dq")
-
-    def test_name_suffix_mds_db_identifier(self):
-        self.assertEqual(self.result['data_ingest']["aws_db_instance.mds_mssql_2012"]["identifier"], "mds-rds-mssql2012-dataingest-apps-preprod-dq")
 
 if __name__ == '__main__':
     unittest.main()
