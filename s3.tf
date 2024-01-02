e107b44fc5b9b6c16c75e7624cb2229ffad39c37
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2024-01-02 12:13:13 +0000

    Fix deprecation warning - bucket logging

--- s3.tf
+++ s3.tf
@@ -14,15 +14,6 @@ resource "aws_s3_bucket" "data_landing_bucket" {
   bucket = "s3-data-landing-${local.naming_suffix}-${random_string.s3.result}"
   # region = data.aws_region.current.name
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.data_landing_bucket_key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
   tags = {
     Name = "s3-data-landing-bucket-${local.naming_suffix}"
   }
@@ -39,6 +30,16 @@ resource "aws_s3_bucket_logging" "data_landing_bucket_logging" {
   bucket        = aws_s3_bucket.data_landing_bucket.id
   target_bucket = var.logging_bucket_id
   target_prefix = "data_landing_bucket/"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_landing_bucket_server_side_encryption_configuration" {
+  bucket = aws_s3_bucket.data_landing_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.data_landing_bucket_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 resource "aws_s3_bucket_metric" "data_landing_bucket_logging" {
